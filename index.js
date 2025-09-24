e1eff05f04d1ea8ebd28f9c0a7ad38fe484e2bb4
ButteredToast01 <isyed0@icloud.com>
2025-09-24 13:15:14 +1000

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function reverseString(str) {
 
 function check() {
   const value = input.value.toLowerCase();
-  const reverse = reverseString(value)
+  const reverse = reverseString(value);
 
   if (value === reverse) {
     alert("P A L I N D R O M E")
